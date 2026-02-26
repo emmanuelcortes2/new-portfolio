701f3609430e441f44e3ba107101220df3a85b8e
emmanuelcortes2 <emmanuelcortes2@hotmail.com>
2026-02-26 15:50:41 -0600

    feat(nav): styled navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,12 @@
 export const Nav = () => {
     return (
-        <nav>
-            <h1>Nav</h1>
+        <nav className="nav">
+            <ul className="nav-list">
+                <li className="list_element"><a href="#projects" >Hello</a></li>
+                <li className="list_element"><a href="#projects" >Projects</a></li>
+                <li className="list_element"><a href="#about">About</a></li>
+                <li className="list_element"><a href="#contact">Contact</a></li>
+            </ul>
         </nav>
     )
 }
